25115aa7cb34cdeaf3908468976c7a1ab090ea3f
jadsonmd <jadsonmd@gmail.com>
2022-03-25 16:51:04 -0300

    teste netlify

--- pages/aluno.vue
+++ pages/aluno.vue
@@ -16,7 +16,7 @@
       </template>
       <v-card>
         <v-card-title>
-          <span class="text-h5">Cadastrar Aluno</span>
+          <span class="text-h5">Cadastrar Aluno JMD</span>
         </v-card-title>
         <v-card-text>
           <v-container>
