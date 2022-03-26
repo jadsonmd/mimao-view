18fa61005e8006c193c3d54e21bee2379530fb6d
jadsonmd <jadsonmd@gmail.com>
2022-03-26 15:57:51 -0300

    Atualizando index para teste

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="logo py-1 d-flex justify-center">
+        <img src="/cropped-favicon.png" alt="Teatime" >
+      </v-card>
+      <v-card>
+       <v-card-title class="headline">
+          Gerencie seus treinos com o Mimão.
+       </v-card-title>
+       <v-card-text>
+          <p>Abaixo segue informações de como comprar suplementos ou utilizar o app.</p>
+          <p>Obrigado.</p>
+       </v-card-text>
+    </v-col>
+  </v-row>
+</template>
